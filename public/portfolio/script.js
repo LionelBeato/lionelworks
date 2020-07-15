77c4f2279fb832032998c0cad1492e54c29a4f4e
Lionel Beato <lionel.beato@gmail.com>
2020-07-15 13:35:32 -0400

    pulls data dynamically from the backend!

--- public/portfolio/script.js
+++ public/portfolio/script.js
@@ -19,23 +19,52 @@ let books = [
     },
 ]
 
+// fetch query for API
+
+async function graphQLQuery(url, data){
+    const response = await fetch(url, {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials:'same-origin',
+        headers: {
+            'COntent-Type': 'application/json'
+        },
+        redirect: 'follow',
+        reffererPolicy: 'no-referrer',
+        body: JSON.stringify(data)
+    });
+    return response.json();
+}
+
 // code to insert said books
 
 let shelf = document.querySelector("#shelf-1");
 
-let test = () => {
-    console.log("this works!")
-    books.forEach(book => {
-        console.log(book);
-        let bookDiv = document.createElement("div");
-        bookDiv.className = "book"; 
+// let test = () => {
+//     console.log("this works!")
+//     books.forEach(book => {
+//         console.log(book);
+//         let bookDiv = document.createElement("div");
+//         bookDiv.className = "book"; 
+//         shelf.appendChild(bookDiv);
+//             }
+//         );
+
+// }
+
+// test(); 
+
+graphQLQuery('https://bookcase-deno.herokuapp.com/graphql', {query:'{ allBooks {title} }'})
+.then(res => {
+    console.log(res.data.allBooks);
+    res.data.allBooks.forEach(book => {
+        let bookDiv = document.createElement(`div`)
+        bookDiv.className = "book";
         shelf.appendChild(bookDiv);
-            }
-        );
+    });
 
-}
-
-test(); 
+}); 
 
 
 
