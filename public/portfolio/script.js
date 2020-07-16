f75b6d4fe6e92fccd67acb798d18120ba0621f9a
Lionel Beato <lionel.beato@gmail.com>
2020-07-16 19:53:53 -0400

    adds working drag and drop!

--- public/portfolio/script.js
+++ public/portfolio/script.js
@@ -1,40 +1,21 @@
-
 // code to generate books dynamically
-
-let books = [
-    {
-        title: "lorem",
-        publisher: "lorem",
-        author: "lorem",
-    },
-    {
-        title: "lorem",
-        publisher: "lorem",
-        author: "lorem",
-    },
-    {
-        title: "lorem",
-        publisher: "lorem",
-        author: "lorem",
-    },
-]
 
 // fetch query for API
 
-async function graphQLQuery(url, data){
-    const response = await fetch(url, {
-        method: 'POST',
-        mode: 'cors',
-        cache: 'no-cache',
-        credentials:'same-origin',
-        headers: {
-            'COntent-Type': 'application/json'
-        },
-        redirect: 'follow',
-        reffererPolicy: 'no-referrer',
-        body: JSON.stringify(data)
-    });
-    return response.json();
+async function graphQLQuery(url, data) {
+  const response = await fetch(url, {
+    method: "POST",
+    mode: "cors",
+    cache: "no-cache",
+    credentials: "same-origin",
+    headers: {
+      "COntent-Type": "application/json",
+    },
+    redirect: "follow",
+    reffererPolicy: "no-referrer",
+    body: JSON.stringify(data),
+  });
+  return response.json();
 }
 
 // code to insert said books
@@ -46,7 +27,7 @@ let shelf = document.querySelector("#shelf-1");
 //     books.forEach(book => {
 //         console.log(book);
 //         let bookDiv = document.createElement("div");
-//         bookDiv.className = "book"; 
+//         bookDiv.className = "book";
 //         shelf.appendChild(bookDiv);
 //             }
 //         );
@@ -55,58 +36,51 @@ let shelf = document.querySelector("#shelf-1");
 
 // test();
 
-graphQLQuery('https://bookcase-deno.herokuapp.com/graphql', 
-{query:"{ allBooks {title, id} }"})
-.then(res => {
-    // console.log(res.data.allBooks);
-    res.data.allBooks.forEach(book => {
-        let bookDiv = document.createElement(`div`)
-        bookDiv.className = 'book'; 
-        bookDiv.id = `book-${book.id}`;
-        bookDiv.setAttribute("draggable", "true")
-        shelf.appendChild(bookDiv);   
-    });
 
+graphQLQuery("https://bookcase-deno.herokuapp.com/graphql", {
+  query: "{ allBooks {title, id} }",
+}).then((res) => {
+  console.log(res);
+  res.data.allBooks.forEach((book) => {
+    let bookDiv = document.createElement(`div`);
+    bookDiv.className = "book";
+    bookDiv.id = `book-${book.id}`;
+    bookDiv.setAttribute("draggable", "true");
+    shelf.appendChild(bookDiv);
 
-    let element = document.getElementById(`book-1`);
-    console.log(element)
-    return element; 
-})
-.then(element => console.log(element))
-
-
-
-
-
-function dragover_handler(ev){
-    ev.preventDefault();
-    ev.dataTransfer.dropEffect =  "move"; 
-}
-
-
-function drop_handler(ev){
-    ev.preventDefault();
-   const data =  ev.dataTransfer.getData("text/plain");
-   console.log(ev);
-   ev.target.appendChild(document.getElementById(data));
-}
-
-
-window.addEventListener('DOMContentLoaded',  async () => {
-    // get the element by its id
-    const element = document.getElementById(`book-1`);
-     await console.log(element)
-    // add the ondragstart event listener
-    element.addEventListener("dragstart", dragstart_handler); 
+    // event listener needs to be added here so that its dynamically placed on every new book element
+    const element = document.getElementById(`book-${book.id}`);
+    element.addEventListener("dragstart", dragstart_handler);
+  });
 });
 
+// window.addEventListener("DOMContentLoaded", (event) => {
+//   // get the element by its id
+//   const element = document.getElementById(`book-4`);
+//   console.log(element);
+//   // add the ondragstart event listener
+//   element.addEventListener("dragstart", dragstart_handler);
+// });
 
+function dragover_handler(ev) {
+  ev.preventDefault();
+  ev.dataTransfer.dropEffect = "move";
+}
 
-function dragstart_handler(ev){
-    // transfer element id to data transfer object
-    ev.dataTransfer.setData("text/plain", ev.target.id);
-    ev.dataTransfer.setData("text/html", ev.target.outerHTML); 
-    ev.dataTransfer.dropEffect = "move";
-    console.log(ev);
+// I need to specify the element here to prevent event handling on nested element
+// see: https://stackoverflow.com/questions/28203585/prevent-drop-inside-a-child-element-when-drag-dropping-with-js/28203782#28203782
+function drop_handler(ev, el) {
+  ev.preventDefault();
+  const data = ev.dataTransfer.getData("text/plain");
+  console.log(data);
+  el.appendChild(document.getElementById(data));
+}
 
+function dragstart_handler(ev) {
+  // transfer element id to data transfer object
+  ev.dataTransfer.setData("text/plain", ev.target.id);
+  console.log(ev);
+
+  ev.dataTransfer.setData("text/html", ev.target);
+  ev.dataTransfer.dropEffect = "move";
 }
